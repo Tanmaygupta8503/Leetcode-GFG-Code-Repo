433e2b7090f925afd2c6d1eccdc7d3e0cf378fde
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-05-19 22:33:24 +0530

    Time: 18 ms (13.74%), Space: 58.4 MB (6.11%) - LeetHub

---
+++ 3307-find-the-maximum-sum-of-node-values/3307-find-the-maximum-sum-of-node-values.java
@@ -0,0 +1,15 @@
+class Solution {
+    Long dp [][];
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        dp = new Long[nums.length + 1][2];
+        return maxSumOfNodes(0 , 1 , nums , k);
+    }
+    private long maxSumOfNodes(int index, int isEven, int[] nums, int k) {
+        if (index == nums.length)
+            return isEven == 1 ? 0 : Integer.MIN_VALUE;
+        if(dp[index][isEven] != null) return dp[index][isEven];
+        long noXorDone = nums[index] + maxSumOfNodes(index + 1, isEven, nums, k);
+        long xorDone = (nums[index] ^ k) + maxSumOfNodes(index + 1, isEven ^ 1, nums, k);
+        return dp[index][isEven] = Math.max(xorDone, noXorDone);
+    }
+}
