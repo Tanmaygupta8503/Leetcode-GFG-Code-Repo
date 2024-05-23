2ae7ccdecdfbf6d732aeef69d5583790c9c6c543
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-05-23 18:45:17 +0530

    Update 1021-distribute-coins-in-binary-tree.cpp

--- 1021-distribute-coins-in-binary-tree/1021-distribute-coins-in-binary-tree.cpp
+++ 1021-distribute-coins-in-binary-tree/1021-distribute-coins-in-binary-tree.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+int move = 0;
+    int distributeCoins(TreeNode* root) {
+        solve(root);
+        return move; 
+    }
+    int solve(TreeNode *root) {
+        if(root == nullptr) return 0;
+        auto left = solve(root->left);
+        auto right = solve(root->right);
+        int extraCoin = left + right + root->val - 1; // extra coin
+        move += abs(extraCoin); //move
+        return extraCoin;
+    }
+};
+class Solution2 {
+public:
     int distributeCoins(TreeNode* root) {
         return solve(root).second;
     }
