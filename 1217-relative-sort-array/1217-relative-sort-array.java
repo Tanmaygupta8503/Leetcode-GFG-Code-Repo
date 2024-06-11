b2e296ffb31f30d891e7209ec6ca95e663d475c2
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-06-11 23:29:47 +0530

    Time: 6 ms (8.52%), Space: 42 MB (45.23%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,20 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer, Integer> countMap = new HashMap<>();
-        List<Integer> remaining = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
-        for (int num : arr2)
-            countMap.put(num, 0);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr2.length; i++) 
+            map.put(arr2[i], i);
         for (int num : arr1) 
-            if (countMap.containsKey(num)) 
-                countMap.put(num, countMap.get(num) + 1);
-            else 
-                remaining.add(num);
-        Collections.sort(remaining);
-        for (int num : arr2)
-            for (int j = 0; j < countMap.get(num); j++)
-                result.add(num);
-        result.addAll(remaining);
-        return result.stream().mapToInt(Integer::intValue).toArray();
+            if (!map.containsKey(num)) 
+                map.put(num, 1000000000);
+        Comparator<Integer> comparator = (num1, num2) -> {
+            if (map.get(num1).equals(map.get(num2))) {
+                return Integer.compare(num1, num2);
+            } else {
+                return Integer.compare(map.get(num1), map.get(num2));
+            }
+        };
+        Integer[] arr1Integer = Arrays.stream(arr1).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr1Integer, comparator);
+        return Arrays.stream(arr1Integer).mapToInt(Integer::intValue).toArray();
     }
 }
