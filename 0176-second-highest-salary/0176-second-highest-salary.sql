33108d244954623ce36df00a8eebf6acf7075208
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-07-12 01:29:53 +0530

    Time: 255 ms (60.75%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,15 @@
-SELECT 
-    CASE 
-        WHEN COUNT(*) < 2 THEN NULL
-        ELSE MIN(salary)
-    END AS SecondHighestSalary
-FROM (
-    SELECT distinct salary
-    FROM employee
-    ORDER BY salary DESC
-    LIMIT 2
-) AS emp;
+-- SELECT 
+--     CASE 
+--         WHEN COUNT(*) < 2 THEN NULL
+--         ELSE MIN(salary)
+--     END AS SecondHighestSalary
+-- FROM (
+--     SELECT distinct salary
+--     FROM employee
+--     ORDER BY salary DESC
+--     LIMIT 2
+-- ) AS emp;
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM employee
+WHERE salary < (SELECT MAX(salary) FROM employee);
