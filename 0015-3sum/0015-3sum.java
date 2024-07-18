4987507181e03ac037c70c0eed4ac2bf7ce9bd74
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-07-18 12:17:07 +0530

    Time: 29 ms (74.46%), Space: 51.2 MB (72.75%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
@@ -20,5 +20,35 @@ class Solution {
             }
         }
         return new ArrayList<>(ans);
+    }
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0 ; i < n ; i ++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int low = i + 1;
+            int high = n - 1;
+            int sum = -1*nums[i];
+            while(low < high) {
+                if(sum - (nums[low] + nums[high]) == 0) {
+                    ans.add(Arrays.asList(-sum , nums[low] , nums[high]));
+                    while (low < high && nums[low] == nums[low + 1]) low ++;
+                    while (low < high && nums[high] == nums[high - 1]) high --;
+                    low ++;
+                    high --;
+                }
+                else if(sum > nums[low] + nums[high]){
+                    low ++;
+                }
+                else{
+                    high --;
+                }
+            }
+        }
+        return ans;
     }
 }
