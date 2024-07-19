790514829dfa6d15e6c7b3339394cc227800d17d
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-07-19 22:51:00 +0530

    Time: 2 ms (98.91%), Space: 54.8 MB (70.14%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.java
@@ -1,29 +1,29 @@
 class Solution1 {
-    Map<String , Integer> map = new HashMap<>();
+    Map<String , Integer> map;
     public int maxScore(int[] cardPoints, int k) {
-        return solve(cardPoints , 0 , cardPoints.length - 1 , k);
+        map = new HashMap<>();
+        return solve(cardPoints , k , 0 , cardPoints.length - 1);
     }
-    public int solve(int arr[] , int i , int j , int k) {
+    private int solve(int arr[] , int k , int i , int j) {
         if(k == 0) return 0;
-        if(i > j) return 0;
-        String key = i + " " + j + " " + k;
-        if(map.containsKey(key)) {
-            return map.get(key);
-        }
-        int op1 = arr[i] + solve(arr , i + 1 , j , k - 1);
-        int op2 = arr[j] + solve(arr , i , j - 1 , k - 1);
+        if(i > j) return Integer.MIN_VALUE;
+        String key = i + "#" + j + "#" + k;
+        if(map.get(key) != null) return map.get(key); 
+        int op1 = arr[i] + solve(arr , k - 1 , i + 1 , j);
+        int op2 = arr[j] + solve(arr , k - 1 , i , j - 1);
         map.put(key , Math.max(op1 , op2));
-        return map.get(key);
+        return Math.max(op1 , op2);
     }
 }
 
 
-
+// find those n - k  elements which are minimum in sum
+// then the answer of total sum - min sum of window will be maximum
 class Solution{
     public int maxScore(int[] cardPoints, int k) {
-        int arrsum = 0;
+        int totalSum = 0;
         for(var i : cardPoints) {
-            arrsum += i;
+            totalSum += i;
         }
         int window = cardPoints.length - k;
         int ws = 0;
@@ -31,12 +31,12 @@ class Solution{
         for(int i = 0 ; i < window ; i ++) {
             sum += cardPoints[i];
         }
-        int min = sum;
+        int windowSum = sum;
         for(int we = window ; we < cardPoints.length ; we ++){
             sum += cardPoints[we];
             sum -= cardPoints[ws ++];
-            min = Math.min(sum , min);
+            windowSum = Math.min(sum , windowSum);
         }
-        return arrsum - min;
+        return totalSum - windowSum;
     }
 }
