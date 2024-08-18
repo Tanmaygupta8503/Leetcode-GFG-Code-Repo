2b7c4b89b1c6d38a801dd563e8b22df1868bba0f
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-08-18 13:04:36 +0530

    Time: 9 ms (54.75%), Space: 86.5 MB (18.99%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.java
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.java
@@ -24,26 +24,61 @@ class Solution1 {
 
 
 
+class Solution2 {
+    public long maxPoints(int[][] point) {
+        int n = point.length;
+        int m = point[0].length;
+        long points[][] = new long[n][m];
+        for(int i = 0 ; i < n ; i ++) {
+            for(int j = 0 ; j < m ; j ++) {
+                points[i][j] = point[i][j];
+            }
+        }
+        for (int row = 1; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                long maxVal = Long.MIN_VALUE;
+                for (int k = 0; k < m; k++) {
+                    maxVal = Math.max(maxVal, points[row - 1][k] - Math.abs(col - k));
+                }
+                points[row][col] += maxVal;
+            }
+        }
+        long max = 0;
+        for (int i = 0; i < m; i++) {
+            max = Math.max(max, points[n - 1][i]);
+        }
+        return max;
+    }
+}
+
+
 class Solution {
-    public long maxPoints(int[][] points) {
-        int cols = points[0].length;
-        long[] currentRow = new long[cols], previousRow = new long[cols];
-        for (int[] row : points) {
-            long runningMax = 0;
-            for (int col = 0; col < cols; ++col) {
-                runningMax = Math.max(runningMax - 1, previousRow[col]);
-                currentRow[col] = runningMax;
-            }
-            for (int col = cols - 1; col >= 0; --col) {
-                runningMax = Math.max(runningMax - 1, previousRow[col]);
-                currentRow[col] = Math.max(currentRow[col], runningMax) + row[col];
-            }
-            previousRow = currentRow;
+    public long maxPoints(int[][] point) {
+        int m = point.length;
+        int n = point[0].length;
+        long prev [] = new long[n];
+        for(int i = 0 ; i < n ; i ++) {
+            prev[i] = point[0][i];
         }
-        long maxPoints = 0;
-        for (int col = 0; col < cols; ++col) {
-            maxPoints = Math.max(maxPoints, previousRow[col]);
+        for(int i = 1 ; i < m ; i ++) {
+            long left[] = new long[n];
+            long right[] = new long[n];
+            left[0] = prev[0];
+            for(int j = 1 ; j < n ; j ++) {
+                left[j] = Math.max(prev[j] , left[j - 1] - 1);
+            }
+            right[n - 1] = prev[n - 1];
+            for(int j = n - 2 ; j >= 0 ; j --) {
+                right[j] = Math.max(prev[j] , right[j + 1] - 1);
+            }
+            long curr[] = new long[n];
+            for(int j = 0 ; j < n ; j ++) {
+                curr[j] = point[i][j] + Math.max(left[j] , right[j]);
+            }
+            prev = curr;
         }
-        return maxPoints;
+        long max = 0;
+        for(long i : prev) max = Math.max(i , max);
+        return max;
     }
 }
