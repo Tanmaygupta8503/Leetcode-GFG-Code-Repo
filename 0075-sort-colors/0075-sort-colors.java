fe412118dfeabba25493b875baf60de30c66ad58
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-09-01 14:12:06 +0530

    Time: 0 ms (100%), Space: 42.2 MB (9.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,29 +1,30 @@
 class Solution {
     public void sortColors(int[] nums) {
         int low = 0;
-        int high = nums.length - 1;
         int mid = 0;
+        int high = nums.length - 1;
         while(mid <= high) {
             switch(nums[mid]) {
                 case 0 -> {
-                    // swap low and mid;
-                    int t = nums[low];
-                    nums[low ++] = nums[mid];
-                    nums[mid ++] = t;
+                    // if mid has 0
+                    int low_val = nums[low]; // save low value
+                    nums[low ++] = nums[mid]; // copy midvalue in
+                    nums[mid ++] = low_val; // put low's val in mids place
                 }
                 case 1 -> {
-                    mid ++;
+                    mid ++; // mid has 1 them forward the pointer
                 }
                 case 2 -> {
-                    //swap high and mid
-                    int t = nums[mid];
-                    nums[mid] = nums[high];
-                    nums[high --] = t;
+                    // mid has 2
+                    int high_val = nums[high];
+                    nums[high --] = nums[mid];
+                    nums[mid] = high_val;
                 }
                 default -> {
-                    throw new IllegalArgumentException("number must be between 0 and 2.");
-                } 
+                    throw new IllegalArgumentException("number has value other than 0 , 1 or 2");
+                }
             }
         }
+        return;
     }
 }
