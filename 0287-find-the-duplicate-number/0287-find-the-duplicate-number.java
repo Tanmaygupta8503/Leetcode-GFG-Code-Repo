626429d595ca4a7e87541cea17edbbc515637483
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-09-03 10:49:12 +0530

    Time: 35 ms (11.3%), Space: 58.4 MB (30.12%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,51 +1,9 @@
-//O(nlogn) complexity using sorting and constant extra space
-class Solution1 {
+class Solution {
     public int findDuplicate(int[] nums) {
         Arrays.sort(nums);
         for(int i = 1 ; i < nums.length ; i ++) {
-            if(nums[i] == nums[i - 1]) {
-                return nums[i];
-            }
-        }   
+            if(nums[i] == nums[i - 1]) return nums[i];
+        }
         return -1;
-    }
-}
-
-//O(n) cyclic sort + O(1) extra space
-class Solution2 {
-    public int findDuplicate(int[] arr) {
-        int i = 0;
-        while (i < arr.length) {
-            int correctIdx = arr[i] - 1;
-            if (arr[i] != arr[correctIdx]) {
-                int temp = arr[i];
-                arr[i] = arr[correctIdx];
-                arr[correctIdx] = temp;
-            } else {
-                if (i != correctIdx) {
-                    return arr[i];
-                }
-                i++;
-            }
-        }
-        return -1; 
-    }
-}
-
-
-class Solution {
-    public int findDuplicate(int[] arr) {
-        int fast = arr[0];
-        int slow = arr[0];
-        do{
-            fast = arr[arr[fast]];
-            slow = arr[slow];
-        }while(fast != slow);
-        fast = arr[0];
-        while(slow != fast) {
-            fast = arr[fast];
-            slow = arr[slow];
-        }
-        return fast;
     }
 }
