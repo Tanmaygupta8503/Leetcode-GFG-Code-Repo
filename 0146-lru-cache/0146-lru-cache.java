ba9e6954ec63956985476eac7151d8f874a50bc7
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-11-22 10:19:40 +0530

    Time: 52 ms (25.57%), Space: 114.9 MB (44.31%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,34 +1,32 @@
-class Node{
-    int key , value;
-    Node next , prev;
-    public Node (int key , int value){
-        this.key = key;
-        this.value = value;
-        this.next = this.prev = null;
-    }
-}
 class LRUCache {
-    
+    class Node{
+        int key , val;
+        Node next , prev;
+        Node(int key , int val){
+            this.key = key;
+            this.val = val;
+            this.next = this.prev = null;
+        }
+    }
     int capacity;
-    Node head = new Node(-1 , -1);
-    Node tail = new Node(-1 , -1);
-    Map <Integer , Node> map = new HashMap<>();
+    Node head = new Node(-1,-1);    
+    Node tail = new Node(-1,-1);
+    Map<Integer , Node> map = new HashMap<>();
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        head.next = tail;
-        tail.prev = head;
+        this.head.next = tail;
+        this.tail.prev = head;
     }
     
     public int get(int key) {
         if(map.containsKey(key)){
             Node node = map.get(key);
-            int ans = node.value;
-            delete(node);
-            insert(node);
+            int ans = node.val;
+            deleteNode(node); // remove from in between the cache in O(1) time
+            insertNode(node); // add it to the first position of cache
             return ans;
-        }
-        else{
+        }else {
             return -1;
         }
     }
@@ -36,23 +34,38 @@ class LRUCache {
     public void put(int key, int value) {
         Node node = new Node(key , value);
         if(map.containsKey(key)){
-            delete(map.get(key));
-        } else if(map.size() == this.capacity){
-            delete(tail.prev);
+            deleteNode(map.get(key));
         }
-        insert(node);
+        else if(map.size() == this.capacity){
+            deleteNode(tail.prev); // tail is (-1 , -1) so the prev of the node will be the element to be removed
+        }
+        insertNode(node);
     }
-    public void insert(Node node){
+    public void insertNode(Node node) {
         map.put(node.key , node);
-        node.next = head.next;
-        head.next = node;
+        node.next = head.next; // (head) --> (head.next) 
+        // node --> node.next connect the node with head's list as node.next = null
+        head.next = node; // now connect the head.next pointer to node;
         node.next.prev = node;
         node.prev = head;
-    } 
-    public void delete(Node node){
+
+        // step 1: a ---> b
+        //         h and  hn
+        // node ----> b <---- a
+        // step 2: a ----> node ----> b
+        // step 3: node.next.prev i.e b prev to node
+        // step 4: node.prev = head i.e set prev node of node from null to head
+    }
+    public void deleteNode(Node node){
         map.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
+        // step 0: np <===> n <===> nn
+        // step 1: np ---> nn <--- n
+        //         |               |
+        //          ---------------
+        // step 2: node.next.prev = node.prev
+        // np <===> nn
     }
 }
 
