bbe07b50bf5c539250122bc62853bc89c5b1030c
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-11-26 14:11:55 +0530

    Time: 31 ms (74.15%), Space: 63 MB (51.03%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
     Long dp[][];
     public long maxAlternatingSum(int[] nums) {
         dp = new Long [nums.length][2];
@@ -22,5 +22,19 @@ class Solution {
             op2 = solve(nums , i + 1 , sign);
         }
         return dp[i][sign ? 1 : 0] = Math.max(op1 , op2);
+    }
+}
+
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        int n = nums.length;
+        long[][] dp = new long[n][2];
+        dp[n - 1][0] = 0;
+        dp[n - 1][1] = nums[n - 1]; 
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i][1] = Math.max(nums[i] + dp[i + 1][0], dp[i + 1][1]);
+            dp[i][0] = Math.max(-nums[i] + dp[i + 1][1], dp[i + 1][0]);
+        }
+        return dp[0][1];
     }
 }
