0a66956009ac36baceecd67799db6518e8a070bf
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2024-12-10 18:25:30 +0530

    Time: 7 ms (16.38%), Space: 110.9 MB (8.54%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.java
+++ 3427-special-array-ii/3427-special-array-ii.java
@@ -18,7 +18,7 @@ class Solution1 {
     }
 }
 
-class Solution {
+class Solution2 {
     public boolean[] isArraySpecial(int[] nums, int[][] queries) {
         int[] validEndPoint = new int[nums.length];
         int i = 0 , j = 0 , n = nums.length;
@@ -41,5 +41,40 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        List<Integer> violatingIndex = new ArrayList<>();
+        for(int i = 1 ; i < nums.length ; i ++){
+            if(nums[i] % 2 == nums[i - 1] % 2) {
+                violatingIndex.add(i);
+            }
+        }
+        boolean[] ans = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            if(!checkIfViolatingIndexPresentInRange(start + 1 , end , violatingIndex)){
+                ans[i] = true;
+            }
+        }
+        return ans;
+    }
+    private boolean checkIfViolatingIndexPresentInRange(int start , int end , List<Integer> violatingIndex) {
+        int low = 0, high = violatingIndex.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midValue = violatingIndex.get(mid);
+            if (midValue >= start && midValue <= end) {
+                return true;
+            } else if (midValue < start) {
+                low = mid + 1;
+            } else { 
+                high = mid - 1; 
+            }
+        }
+        return false;
     }
 }
