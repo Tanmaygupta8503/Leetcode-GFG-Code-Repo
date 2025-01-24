8630e946a620a3b7992b1f835c195e137362050e
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2025-01-25 01:15:27 +0530

    Time: 28 ms (21.31%), Space: 55.7 MB (24.57%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,39 +1,36 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0 ; i < graph.length ; i ++){
-            adj.add(new ArrayList<>());
+        List<List<Integer>> revGraph = new ArrayList<>();
+        int n = graph.length;
+        for(int i = 0 ; i < n ; i ++){
+            revGraph.add(new ArrayList<>());
         }
-        for(int i = 0 ; i < graph.length ; i ++) {
-            for(int j : graph[i]) {
-                adj.get(j).add(i);
-            }
-        }
-        var ans = topoSort(adj);
-        Collections.sort(ans);
-        return ans;
-    }
-    List<Integer> topoSort(ArrayList<ArrayList<Integer>> adj) {
-        int v = adj.size();
-        int indegree[] = new int[v];
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int i = 0 ; i < v ; i ++) {
-            for(int neigh : adj.get(i)){
-                indegree[neigh] ++;
+        int indegree[] = new int[n];
+        for(int i = 0 ; i < n ; i ++){
+            for(int j : graph[i]){
+                revGraph.get(j).add(i);
+                indegree[i]++;
             }
         }
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0 ; i < v ; i ++) {
-            if(indegree[i] == 0) q.add(i);
-        } 
-        while(!q.isEmpty()) {
-            int node = q.poll();
-            ans.add(node);
-            for(int neigh : adj.get(node)) {
-                indegree[neigh] --;
-                if(indegree[neigh] == 0) q.add(neigh);
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i < n; i ++){
+            if(indegree[i] == 0){
+                q.add(i);
+                list.add(i);
             }
         }
-        return ans;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(var neigh : revGraph.get(node)){
+                indegree[neigh] --;
+                if(indegree[neigh] == 0){
+                    q.add(neigh);
+                    list.add(neigh);
+                }
+            }
+        }
+        Collections.sort(list);
+        return list;
     }
 }
