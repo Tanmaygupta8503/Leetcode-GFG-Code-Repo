d5d6002dc9b28b5cd5658e36f48c7e641c5edbba
Tanmay Gupta <96345346+Tanmaygupta8503@users.noreply.github.com>
2025-01-30 11:26:20 +0530

    Time: 0 ms (100%), Space: 44.4 MB (68.05%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -18,13 +18,11 @@ class Solution {
         HashMap<Node , Node> nodeToRandomPointer = new HashMap<>();
         Node dummy = head;
         while(dummy != null) {
-            // make a copy of each node
             nodeToRandomPointer.put(dummy , new Node(dummy.val));
             dummy = dummy.next;
         }
         dummy = head;
         while(dummy != null) {
-            // now just iterate and put the next and random of them
             Node copyNode = nodeToRandomPointer.get(dummy);
             copyNode.next = nodeToRandomPointer.get(dummy.next);
             copyNode.random = nodeToRandomPointer.get(dummy.random);
